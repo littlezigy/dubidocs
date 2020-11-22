5a345dd820d4f78d54dbc63879855e8f0b4d6836
Adesuwa T. Dawodu <littlezigy@gmail.com>
2020-11-22 22:04:41 +0100

    Add cursor functions

--- src/components/cursors.js
+++ src/components/cursors.js
@@ -7,29 +7,27 @@ const cursorColors = [
     'purple'
 ];
 
+const recursiveRandNo = (cursors, counter) => {
+    let x  = Math.ceil(Math.random() * (cursorColors.length - 1));
+
+    console.log('Candidate: X', x);
+    console.log('SELECTED CURSORS', selectedCursorColors);
+    console.log(!selectedCursorColors.includes(x) )
+    counter++;
+    if(counter >= 10)
+        return x;
+    if(cursors.length > cursorColors.length || !selectedCursorColors.includes(x) ) {
+        selectedCursorColors.push(x);
+        console.log('RETURNING X NOW');
+        return x;
+    }
+    else return recursiveRandNo();
+}
+
+
 const randomizeCursorColors = function( cursors ) {
     let counter = 1;
-    console.log('CURSOR', counter);
-    let chooseRandNo = () => {
-        let x  = Math.ceil(Math.random() * (cursorColors.length - 1));
-
-        console.log('...... Choosing Random number ......');
-        console.log('Candidate: X', x);
-        console.log('SELECTED CURSORS', this.selectedCursorColors);
-        console.log(!selectedCursorColors.includes(x) )
-        counter++;
-        if(counter >= 10)
-            return x;
-        if(cursors.length > cursorColors.length || !selectedCursorColors.includes(x) ) {
-            selectedCursorColors.push(x);
-            console.log('RETURNING X NOW');
-            return x;
-        }
-        else return chooseRandNo();
-    }
-    
-    let randomNumber = chooseRandNo();
-    console.log('RETURNED RANDOME NUMBER', randomNumber);
+    let randomNumber = recursiveRandNo(cursors, counter);
     return `border-color: ${ cursorColors[randomNumber] }`;
 }
 
