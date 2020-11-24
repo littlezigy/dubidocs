be47bde68e725479c59dd5e2d4851ed3d24481f4
Adesuwa T. Dawodu <littlezigy@gmail.com>
2020-11-24 05:39:49 +0100

    Fix fetchDocuments function

--- src/components/fetchDocuments.js
+++ src/components/fetchDocuments.js
@@ -1,11 +1,35 @@
-let docSeed;
+import { SkynetClient, genKeyPairFromSeed } from "skynet-js";
+import db from './skydbKeys.json';
 
+// let docKeys;
+
+
+/*
 const checkPermissions = function() {
+}
+*/
+
+const fetchDocKeys = function() {
+    // Get doc id in url
+    console.log('doc id i url', this.$store.state.docID);
 }
 
 // Fetches all documents
-export default function() {
-    if(!docSeed)
-        docSeed = window.localStorage.getItem('docID');
-    console.log('DOC SED', docSeed);
+export const fetchAllDocuments = function(user) {
+    let portal = window.localStorage.portal;
+    const client = new SkynetClient(portal);
+    const { publicKey } = genKeyPairFromSeed(user);
+
+    const key = db.userDocs;
+    return client.db.getJSON(publicKey, key)
+    .then(res => {
+        console.log('GOTTEN USER DOCUMENTS', res);
+        if(!res)
+            return null;
+        else return res.data;
+    });
+}
+
+export const fetchDocument = function() {
+    return fetchDocKeys()
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <p>Go back to editing...</p>
+
+      <button @click = 'newDocument'>New Document</button>
+
+      <div id = 'documents'>
+          <div v-for = '( doc, id,index) in docs' :key = 'index'
+               @click = 'goToEditor(id)'
+          >
+              <p class = 'title'>{{ doc.title || 'Untitled Document' }}</p>
+
+              <p class = 'date' v-if = 'doc.created'>Created on {{ doc.created }}</p>
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { fetchAllDocuments } from '@/components/fetchDocuments';
+import { createDocument } from '@/components/createDocument';
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+    name: 'Home',
+    data(){
+        return {
+            docs: {}
+        }
+    },
+    methods: {
+        newDocument() {
+            return createDocument(this.$store.state.user)
+            .then(res => {
+                console.log('CREATED DOC', res);
+                this.$router.push({ name: 'Editor', params: { docID: res } });
+            });
+        },
+        goToEditor(id) {
+            this.$router.push({ name: 'Editor', params: { docID: id } });
+        }
+    },
+    mounted() {
+        return fetchAllDocuments(this.$store.state.user)
+        .then(res => {
+            if(res) {
+                for (const key in res) {
+                    let r = res[key]
+                    console.log('RR', r);
+                    this.docs[key] = r;
+                    if(r.created)
+                        this.docs[key].created = r.created.toLocaleString();
+                }
+            }
+            console.lgo('DOCS', this.docs);
+            console.log('FETHCED THOS DOCS BOSS', res);
+        });
+    }
 }
 </script>
